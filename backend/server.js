f2d23224f5618247561d3dfbc99ed379ae8fad70
Abai369 <jcaleb1110@gmail.com>
2025-01-24 21:13:24 -0800

    feat Product.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
-const app = express();
 
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Buffalo Mistico API');
-});
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/buffalo-mistico", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
 
+// Routes
+app.use("/api/products", require("./routes/products"));
+
+// Start server
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
