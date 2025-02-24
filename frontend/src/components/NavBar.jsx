0b029a5541364aecc109f28e02558c924542ccb3
Abai369 <jcaleb1110@gmail.com>
2025-02-24 13:56:27 -0800

    feat social icons and logo

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebook, faTiktok } from '@fortawesome/free-brands-svg-icons';
 import "../styles/NavBar.css";
 
 function NavBar() {
     return (
         <nav className="navbar"> 
          <div className="navbar__logo">
-            <Link to="/">Buffalo Mistico</Link>
+            <img src="/assets/bufalo_mistico-original.jpg" alt="Buffalo Mistico Logo" style={{ height:"80px"}} />
          </div>
          <ul className="navbar__menu">
+            <ul className="social-icons">
+              <li>
+                <a href="https://www.instagram.com/bufalomistico/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
+                </li>
+                <li>
+                <a href="https://www.facebook.com/profile.php?id=100005140448023" target="_blank" rel="noopener noreferer"><FontAwesomeIcon icon={faFacebook} /></a>
+                </li>
+                <li>
+                    <a href="https://www.tiktok.com/@bufalo.mistico" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTiktok} /></a>
+                </li>
+            </ul>
             <li className="navbar__item">
                 <Link to="/">Home</Link>
             </li>
