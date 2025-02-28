253f19295c818c620baf2bfc79aaae4101b5d96d
Abai369 <jcaleb1110@gmail.com>
2025-02-27 19:17:07 -0800

    feat monederos y cosmetiqueros medianos

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 const app = express();
 app.use(cors());
@@ -11,6 +14,27 @@ app.use(bodyParser.json());
 mongoose.connect("mongodb://localhost:27017/buffalo-mistico")
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.log(err));
+
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.dra45ejdt,
+  api_key: process.env.ee61d0c431edca545ff7a914b64df0,
+  api_secret: process.env.tatanka
+});
+
+// Multer configuration
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder : "buffalo-mistico",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"]  }
+});
+const upload = multer({ storage });
+
+// Import Routes
+app.post("/api/upload", upload.single("image"), (req, res) => {
+  res.json({ imageUrl: req.file.path }); // Return the image URL
+});
 
 // Routes
 app.use("/api/products", require("./routes/products"));
